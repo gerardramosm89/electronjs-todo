5f0abb336a453766dba97cd8f9654d8b87154345
Gerard Ramos <gerard.ramosm89@gmail.com>
2017-07-17 13:52:26 -0700

    adding git ignore

--- index.js
+++ index.js
@@ -3,19 +3,35 @@ const electron = require('electron');
 const { app, BrowserWindow, Menu } = electron;
 
 let mainWindow;
+let addWindow;
 
 app.on('ready', () => {
   mainWindow = new BrowserWindow({});
   mainWindow.loadURL(`file://${__dirname}/main.html`);
+  mainWindow.on('closed', () => app.quit());
+
   const mainMenu = Menu.buildFromTemplate(menuTemplate);
   Menu.setApplicationMenu(mainMenu);
 });
+
+function createAddWindow() {
+  addWindow = new BrowserWindow({
+    width: 300,
+    height: 200,
+    title: 'Add New Todo'
+  });
+  addWindow.loadURL(`file://${__dirname}/add.html`);
+}
 
 const menuTemplate = [
   {
     label: 'File',
     submenu: [
-      { label: 'New Todo'},
+      { label: 'New Todo',
+        click() {
+          createAddWindow();
+        }
+      },
       { label: 'Quit',
         accelerator: process.platform === 'darwin' ? 'Command+Q' : 'Ctrl+Q',
         click() {
@@ -25,3 +41,25 @@ const menuTemplate = [
     ]
   }
 ];
+
+if (process.env.NODE_ENV !== 'production') {
+  menuTemplate.push({
+    label: 'View',
+    submenu: [
+      {
+        label: 'Toggle Developer Tools',
+        accelerator: 'Command+J',
+        click(item, focusedWindow) {
+          focusedWindow.toggleDevTools();
+        }
+      }
+    ]
+  });
+}
+
+
+
+
+
+
+
